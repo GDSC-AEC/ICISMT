afadf7ee36829615944b29d3b3b48f3163453290
Jiya-Ull-Haq <shaik.jiyaullhaq@gmail.com>
2022-03-03 14:33:22 +0530

    Version 4 - Master Release

--- assets/JS/request.js
+++ assets/JS/request.js
@@ -1,64 +1,61 @@
 async function httpGet(theUrl)
 {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+  let response = fetch(theUrl);
+  let data = (await response).json();
+  return data
 }
+
 
 async function para(id, data){
     const para = document.createElement("p");
     const i = document.createElement("i");
-    para.innerHTML = "This is a paragraph.";
     i.className = "bx bx-badge-check";
-    i.innerHTML = `${data}`;
+    i.innerHTML = ` ${data}`;
     document.getElementById(id).appendChild(para).appendChild(i);
 }
 
 async function LoadNames(){
     let Odata = await httpGet('https://ismt-dynamic.herokuapp.com/organizing')
-    // let Idata = await httpGet('https://ismt-dynamic.herokuapp.com/international')
-    // let Ndata = await httpGet('https://ismt-dynamic.herokuapp.com/national')
-    // let Tdata = await httpGet('https://ismt-dynamic.herokuapp.com/technical')
+    let Idata = await httpGet('https://ismt-dynamic.herokuapp.com/international')
+    let Ndata = await httpGet('https://ismt-dynamic.herokuapp.com/national')
+    let Tdata = await httpGet('https://ismt-dynamic.herokuapp.com/technical')
 
-    for(let i = 0; i<Odata.length; i++){
-        console.log(Odata[i])
-    }
 
-    // // for Organizing
-    // Odata.forEach(
-    //     function(value, index) {
-    //     if ( index != 0 ) {
-    //         para('Organizing', value)
-    //     }
-    // });
+
+    // for Organizing
+    Odata.forEach(
+        function(value, index) {
+        if ( index != 0 ) {
+            para('Organizing', value)
+        }
+    });
     
 
-    // // for International
-    // Idata.forEach(
-    //     function(value, index) {
-    //     if ( index != 0 ) {
-    //         para('International', value)
-    //     }
-    // });
+    // for International
+    Idata.forEach(
+        function(value, index) {
+        if ( index != 0 ) {
+            para('International', value)
+        }
+    });
 
 
-    // // for National
-    // Ndata.forEach(
-    //     function(value, index) {
-    //     if ( index != 0 ) {
-    //         para('National', value)
-    //     }
-    // });
+    // for National
+    Ndata.forEach(
+        function(value, index) {
+        if ( index != 0 ) {
+            para('National', value)
+        }
+    });
 
 
-    // // for Technical
-    // Tdata.forEach(
-    //     function(value, index) {
-    //     if ( index != 0 ) {
-    //         para('Technical', value)
-    //     }
-    // });
+    // for Technical
+    Tdata.forEach(
+        function(value, index) {
+        if ( index != 0 ) {
+            para('Technical', value)
+        }
+    });
 
 
 }
